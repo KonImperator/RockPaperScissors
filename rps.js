38f79ca2489fc8b0a1b26f1e19fa6d6b4df28ada
KonImperator <nikola_hristov22@abv.bg>
2022-11-17 01:32:58 +0200

    Adding comments

--- rps.js
+++ rps.js
@@ -4,16 +4,22 @@ const rl = require("readline").createInterface({
 });
 
 const computerOptions = { 0: "rock", 1: "paper", 2: "scissors" };
+// options the computer will be able to choose from; keys are 0,1,2 in order to be able to use the Math.random function for randomness
 const validUserChoices = ["rock", "r", "paper", "p", "scissors", "s"];
+// adding all valid choices in order to easily invalidate any other user input
 let victories = 0;
 let losses = 0;
 let draws = 0;
+// initiating variables to be used
+
 
 let reset = function () {
   victories = 0;
   losses = 0;
   draws = 0;
 };
+// reset function will be used to reset the user stats to 0
+
 
 const compareChoices = function (userChoice, computerChoice) {
   switch (computerChoice) {
@@ -61,27 +67,35 @@ const compareChoices = function (userChoice, computerChoice) {
       break;
   }
 };
+// compareChoices function will compare the choices of both the user and the computer and print the outcome of the comparison
+
 
 const rpsGame = function () {
-  rl.question(
-    "Please choose rock/r, paper/p or scissors/s: ",
-    function (userChoice) {
+  rl.question("Please choose rock/r, paper/p or scissors/s: ", function (userChoice) {
       userChoice = userChoice.toLowerCase();
+      // making sure the user's choice is standardized for easy validation
       let computerChoice = computerOptions[Math.floor(Math.random() * 3)].toString();
+      // computer generating it's choice from the intially created object that holds the 3 choices
       if (validUserChoices.includes(userChoice)) {
+        // validating the user's choice
         console.log(`The computer chose....${computerChoice}`);
         compareChoices(userChoice, computerChoice);
+        // comparing 2 choices to see who won
         console.log(
           `Current stats: Victories: ${victories} Losses: ${losses} Draws: ${draws}\nLet's play again!\n`
         );
       } else if (userChoice === "reset") {
         reset();
+        // resetting stats if user typed in "reset"
         console.log("Stats reset\n")
       } else {
         console.log("Invalid input!\n");
+        // if input was found to be invalid, print this
       }
       rpsGame();
+      // recursively calling the function
     }
   );
 };
 rpsGame();
+// this will be the main function
